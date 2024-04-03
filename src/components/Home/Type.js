5e5316f122c39b5657151da5a57c76f3c194e425
Yanchen Zhou <beinu2137@gmail.com>
2024-04-03 00:51:36 -0700

    update npm packages version

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,21 +1,22 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
+// import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+    // <Typewriter
+    //   options={{
+    //     strings: [
+    //       "Software Developer",
+    //       "Freelancer",
+    //       "MERN Stack Developer",
+    //       "Open Source Contributor",
+    //     ],
+    //     autoStart: true,
+    //     loop: true,
+    //     deleteSpeed: 50,
+    //   }}
+    // />
+    <div></div>
   );
 }
 
