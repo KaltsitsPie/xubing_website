30c8b9d9a41732c8c2538a99641a8d01a1cba7b6
Yanchen Zhou <beinu2137@gmail.com>
2024-04-04 02:21:55 -0700

    update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost:8081/";
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost:8081/";
 
-console.log("axios default url: " + axios.defaults.baseURL);
+// console.log("axios default url: " + axios.defaults.baseURL);
 
 export async function apiGetFiles() {
+  const repositoryUrl = "https://api.github.com/repos/KaltsitsPie/personal-site-files/contents/";
+
     try {
-        console.log("index.js 准备 getfiles");
-        const response = await axios.get("/get-files");
+        const response = await axios.get(repositoryUrl);
         let data = response.data;
         data = data.filter(item => item.name.endsWith("-xubing.md"));
+        // console.log("^^^^", data);
         return data;
     } catch (error) {
         console.error("Failed to apiGetFiles", error);
